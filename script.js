2c5e07ba675b9f2ae311a6bdec5d275a3ad00d98
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-10-02 21:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ const inputs = document.querySelectorAll('.code');
+ for (let i = 0; i < inputs.length; i++) {
+	 inputs[i].addEventListener('input', function() {
+		 if (this.value.length === this.maxLength) {
+            inputs[i + 1].focus();
+          }
+		  });
+	  inputs[i].addEventListener('keydown', function(event) {
+          if (event.keyCode === 8 && this.value.length === 0) {
+            inputs[i - 1].focus();
+          }
+        });
+      }
